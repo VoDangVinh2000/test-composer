2e09ca3583facb366b2d623d0f793a12e18d5124
VoDangVinh <01686228222vinh@gmail.com>
2021-03-29 17:48:57 +0700

    update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '3868b80972bb08935746115a16df9cbb751a30f5',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3868b80972bb08935746115a16df9cbb751a30f5',
+    ),
+    'back-end/test-composer' => 
+    array (
+      'pretty_version' => '1.0.0',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3868b80972bb08935746115a16df9cbb751a30f5',
+    ),
+  ),
+);
